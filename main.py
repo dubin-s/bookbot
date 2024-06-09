a626c4fddab099e723334ba347f8e5aa97998b24
Spenser <18bf4e7@proton.me>
2024-06-09 11:30:04 -0400

    format file

--- main.py
+++ main.py
@@ -1,30 +1,35 @@
 def sort_on(dict):
     return dict["num"]
 
+
 def get_char_report(char_count):
     char_report = []
     for char in char_count:
         if char.isalpha():
-            char_report.append({"char": char, "num":char_count[char]})
+            char_report.append({"char": char, "num": char_count[char]})
     char_report.sort(reverse=True, key=sort_on)
     return char_report
+
 
 def get_char_count(text):
     char_count = {}
     for char in text:
         char = char.lower()
-        if(char in char_count):
+        if (char in char_count):
             char_count[char] += 1
         else:
             char_count[char] = 1
     return char_count
 
+
 def get_num_words(text):
     return len(text.split())
+
 
 def get_text(path):
     with open(path) as f:
         return f.read()
+
 
 def main():
     file_path = "books/frankenstein.txt"
